2b9f377e5120159602121782c5efcaa5bf4881a7
ouyang <282229505@qq.com>
2019-03-18 16:57:01 +0800

    完善博客

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import logging
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,9 +78,17 @@ WSGI_APPLICATION = 'blogproject.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '10.2.30.12',
+        'PORT': '3306',
     }
 }
 
@@ -114,7 +123,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -123,4 +132,22 @@ USE_TZ = True
 # SITE_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+    }
+}
 
--- docs.md
+++ docs.md
@@ -1,27 +1,67 @@
 # 项目说明
 
-## Django操作
+> ## Django操作
 
 - 生成项目依赖组件详情文件: <pre>pip freeze > requirements.tx</pre> 该命令会在项目根目录生成 <code>requirements.tx</code> 文件
 
-- 安装项目依赖: <pre>pip install -r requirements.txt</pre>
+- 安装项目依赖:
+<pre>
+pip install -r requirements.txt
+</pre>
 
-- 收集静态文件命令: <pre>python manage.py collectstatic</pre>
+- 收集静态文件命令:
+<pre>
+python manage.py collectstatic
+</pre>
 
-- 创建数据库文件：<pre>python manage.py migrate</pre>
+- 创建数据库文件:
+<pre>
+python manage.py migrate
+</pre>
 
-- 创建后台管理员超级用户：<pre>python manage.py createsuperuser</pre>
+- 更改数据模型后执行数据迁移:
+<pre>
+python manage.py makemigrations
+python manage.py migrate
+</pre>
 
-## Uwsgi操作
+- 创建后台管理员超级用户：
+<pre>
+python manage.py createsuperuser
+</pre>
 
-- 启动django项目：<pre>uwsgi --ini uwsgi.ini</pre>
+- 配置MySQL数据库：
+<pre>
+1.安装mysql模块：pip install pymysql
+2.在项目文件夹中的 __ini__.py 文件处导入mysql模块：
+	import pymysql
+	pymysql.install_as_MySQLdb()
+3.修改 setting.py 文件中的 DATABASES 配置
+</pre>
 
-- 关闭全部进程：<pre>pkill -f uwsgi -9</pre>
+> ## Uwsgi操作
+
+- 安装uwsgi:
+<pre>
+pip install uwsgi
+</pre>
+
+- 启动django项目：
+<pre>
+1.启动：uwsgi --ini uwsgi.ini
+2.重载：uwsgi --reload uwsgi/uwsgi.pid
+</pre>
+
+- 关闭全部进程：
+<pre>
+pkill -f uwsgi -9
+</pre>
 
 
-## nginx配置
+> ## nginx配置
 
-```python
+- 新增 <code>blog.conf</code> 文件，并写入以下配置内容:
+```nginx
 server {
         listen 80;	# 监听端口
         server_name  blog.test;	# 访问域名地址
@@ -39,4 +79,5 @@ server {
         }
 }
 ```
+
 
